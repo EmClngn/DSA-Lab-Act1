14ef4848f80d76e9b861ec2baaf7b49a5e50e1d4
unknown <emgcalangian@gmail.com>
2024-09-20 20:31:44 +0800

    Fixed an error

--- Exercise2.py
+++ Exercise2.py
@@ -4,16 +4,16 @@ while True:
             print("V for Voltage \n""I for current \n""R for Resistance")
             ask = input("What do you want to calculate?: ")
             if ask.upper() == "V":
-                crrnt = float(input("What is the Current(I)?:"))
-                resist = float(input("what is the Resistance(R)?:"))
+                crrnt = float(input("What is the Current(I)?: "))
+                resist = float(input("what is the Resistance(R)?: "))
                 print(f"Voltage: {crrnt * resist}V")
             elif ask.upper() == "I":
-                volt = float(input("What is the Voltage(V)?:"))
-                resist = float(input("What is the Resistance(R)?:"))
-                print(f"Current: {volt/crrnt}A")
+                volt = float(input("What is the Voltage(V)?: "))
+                resist = float(input("What is the Resistance(R)?: "))
+                print(f"Current: {volt/resist}A")
             elif ask.upper() == "R":
-                volt = float(input("What is the Voltage(V)?:"))
-                crrnt = float(input("what is the Current(I)?:"))
+                volt = float(input("What is the Voltage(V)?: "))
+                crrnt = float(input("what is the Current(I)?: "))
                 print(f"Resistance: {volt/crrnt}Ω")
             break
         except ZeroDivisionError:
